f629af2e218ca0b258169d6d2bb35a5be9803239
Dwiqy Fahlavi Muhammad Ramdani <fahlavi.dwiky@upi.edu>
2022-06-24 21:54:02 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.60f2a0549331003a5e86147a507ae587.js"
+  "/mini-project/precache-manifest.f5bc6b3a9baf9fab000d8b7be678f15a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/mini-project/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
