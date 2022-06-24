f18d6c8b796030efae0b501b85fc9afbc9bd0e42
Dwiqy Fahlavi Muhammad Ramdani <fahlavi.dwiky@upi.edu>
2022-06-24 21:42:59 +0700

    Updates

---
+++ precache-manifest.60f2a0549331003a5e86147a507ae587.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "909ed53c896ef7660a03",
+    "url": "/static/css/main.c3159868.chunk.css"
+  },
+  {
+    "revision": "909ed53c896ef7660a03",
+    "url": "/static/js/main.909ed53c.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "52591dfc5f395521d94a",
+    "url": "/static/js/2.52591dfc.chunk.js"
+  },
+  {
+    "revision": "d9ce6d317115bc41b4f8",
+    "url": "/static/js/runtime~main.d9ce6d31.js"
+  },
+  {
+    "revision": "e39625505308fec1b101805ca0f0cbe1",
+    "url": "/index.html"
+  }
+];
